a36bd4e7595a7aa77fa124ed4e82cec704c630e7
sadrda <kofler_a@yahoo.de>
2020-11-19 21:22:15 +0100

    fix(wetter): smoothen error handling

--- endpoints/spiel.js
+++ endpoints/spiel.js
@@ -1,11 +1,14 @@
 const ytdl = require("ytdl-core");
 
 module.exports = async (message) => {
-	if (message.member.voice.channel) {
+	if (message.member) {
 		const splitMessage = message.content.split(" ");
-		if(splitMessage.length >= 2) {
+		if(splitMessage.length > 2) {
 			const connection = await message.member.voice.channel.join();
 			connection.play(ytdl(splitMessage[2], { filter: "audioonly" }));
+		}
+		else {
+			message.reply("Wos solli spielen? Muasch mor schun ah an link geben.");
 		}
 	} else {
 		message.reply("Geahsch mr gian bitte zearsch in a Stimmenkanal inni? Geah dai, seimr so guat.");
--- endpoints/wetter.js
+++ endpoints/wetter.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const {openweathermapKey} = require("../credentials.json");
 module.exports = (message) => {
 	const splitMessage = message.content.split(" ");
-	if(splitMessage.length >= 2) {
+	if(splitMessage.length > 2) {
 		axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${splitMessage[2]}&appid=${openweathermapKey}&units=metric&lang=de`)
 			.then((response) => {
 				const data = response.data;
@@ -16,5 +16,8 @@ Dr Wind blost mit ${data.wind.speed}m/s in ${data.wind.deg}° Richtung. ${data.w
 			.catch(() => {
 				message.reply("I hon leider kuan Wetter fir diar. I frog spater numol ban Stefan Windegger vom Rai Sender Bouzn noch.");
 			});
+	}
+	else {
+		message.reply("Es Wetter fa wos? Epfl? Birnen? Bananen?");
 	}
 };
